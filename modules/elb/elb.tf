6a20021b830a5fefbf4226c7f363a3c4fa3a3784
JeongJu Kim <haje01@gmail.com>
2016-06-14 11:27:46 +0900

    elb healthcheck

--- modules/elb/elb.tf
+++ modules/elb/elb.tf
@@ -29,6 +29,14 @@ resource "aws_security_group" "elb" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "HTTP:80/"
+        interval = 10
+    }
+
     tags {
         Name = "${var.proj_prefix}-elb"
         Desc = "${var.proj_desc}"
